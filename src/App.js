e84cb7a88f3bd3b2f7be6eac900b425553ceb27c
Your Name <darwinmuthu450@gmail.com>
2021-03-19 10:25:46 +0530

    coded for needed data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import Header from "./components/shared/Header";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+  const [cases_time, setCases_time] = useState({});
+  const [stateWise, setStateWise] = useState([]);
+  const [tested, setTested] = useState({});
+
+  useEffect(() => {
+    const dataSetter = (data) => {
+      setCases_time(data.cases_time_series[data.cases_time_series.length - 1]);
+      setStateWise(data.statewise);
+      setTested(data.tested[data.tested.length - 1]);
+    };
+    axios
+      .get("https://api.covid19india.org/data.json")
+      .then((res) => {
+        dataSetter(res.data);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
